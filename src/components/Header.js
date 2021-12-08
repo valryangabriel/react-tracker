3466d4b0b4080dfaf0eeb57a0fd27d7f7ecd130f
valryangabriel <gabrielvrcode@gmail.com>
2021-12-08 08:28:36 -0600

    Initial Commit

---
+++ src/components/Header.js
@@ -0,0 +1,23 @@
+import PropTypes from "prop-types";
+import Button from "./Button"
+
+//rafce
+const Header = ({ title }) => {
+  return (
+    <header className="header">
+      <h1>{title}</h1>
+      <Button color="green" text="Add" />
+    </header>
+  )
+}
+
+Header.defaultProps = {
+  title: "Task Tracker",
+}
+
+Header.propTypes = {
+  title: PropTypes.string.isRequired,
+}
+
+//rafce
+export default Header
